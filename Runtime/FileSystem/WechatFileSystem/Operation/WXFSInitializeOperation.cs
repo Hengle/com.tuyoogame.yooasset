55fd3236677f3620ab05fe182b7f92f74e4ee87d
Blank <wangfj11@foxmail.com>
2024-08-19 17:57:03 +0800

    [增加]1. 增加微信文件系统适配

---
+++ Runtime/FileSystem/WechatFileSystem/Operation/WXFSInitializeOperation.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_WEBGL && WECHAT_MINI_GAME
+using YooAsset;
+
+internal partial class WXFSInitializeOperation : FSInitializeFileSystemOperation
+{
+    private readonly WechatFileSystem _fileSystem;
+
+    public WXFSInitializeOperation(WechatFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+    internal override void InternalOnStart()
+    {
+        Status = EOperationStatus.Succeed;
+    }
+    internal override void InternalOnUpdate()
+    {
+    }
+}
+#endif
